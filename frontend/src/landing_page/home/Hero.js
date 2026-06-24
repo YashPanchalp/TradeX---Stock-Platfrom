debf8eb3dd0fa3c4b41123318d0ddcd7411fdfb4
Yash Panchal <776yashp@gmail.com>
2026-06-24 16:29:13 +0530

    Fixed frontend route error

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 function Hero() {
+    const dashBoardSignUpUrl = `${process.env.REACT_APP_DASHBOARD_URL}/signup`;
     return ( 
         <div className='container p-5 mb-5'>
             <div className='row text-center'>
                 <img src = 'media/images/homeHero.png' alt = 'Hero Image' className='mb-5' />
                 <h1 className='mt-5'>Invest in everything</h1>
                 <p>Online platform to invest in stocks, derivatives , mutual funds and more</p>
-                <Link to="http://localhost:3000/signup" className='p-2 btn btn-primary fs-5 signup-cta'
+                <Link to={dashBoardSignUpUrl} className='p-2 btn btn-primary fs-5 signup-cta'
                 style={{margin: "0 auto", color:"white"
                 }}>Signup Now</Link>
             </div>
